73a3d2ef0aadc9c48d28cc7f75e2eee0730ab2cd
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-12-16 19:23:52 +0000

    Add successful far plane vertex calculation

--- Assets/Scripts/EffectProjection.cs
+++ Assets/Scripts/EffectProjection.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Vector3 = UnityEngine.Vector3;
 
 public class EffectProjection : MonoBehaviour
 {
     private Vector3[] closeCamVertices;
+    private Vector3[] farCamVertices;
     private Camera cam;
+    float zDistance;
+    float yDistance;
+    float xDistance;
 
     private void Start()
     {
         cam = gameObject.GetComponent<Camera>();
-        Gizmos.color = Color.magenta;
+        Gizmos.color = Color.yellow;
+
+        zDistance = cam.farClipPlane;
+        yDistance = Mathf.Tan(cam.fieldOfView * Mathf.Deg2Rad /2) * zDistance;
+        xDistance = Mathf.Tan(Camera.VerticalToHorizontalFieldOfView(cam.fieldOfView, cam.aspect) * Mathf.Deg2Rad /2) * zDistance;
     }
 
     private void Update()
     {
         closeCamVertices = CalculateCloseCamVertices();
+        farCamVertices = CalculateFarCamVertices();
     }
 
     private Vector3[] CalculateCloseCamVertices()
@@ -29,12 +40,25 @@ public class EffectProjection : MonoBehaviour
         return new Vector3[4] {near_topLeft, near_topRight, near_bottomRight, near_bottomLeft};
     }
 
-    /*private void OnDrawGizmos()
+    private Vector3[] CalculateFarCamVertices()
     {
-        foreach(Vector3 vertex in closeCamVertices)
+        Vector3 far_topLeft = closeCamVertices[0] + (-xDistance * transform.right) + (yDistance * transform.up) + (zDistance * transform.forward);
+        Vector3 far_topRight = closeCamVertices[0] + (xDistance * transform.right) + (yDistance * transform.up) + (zDistance * transform.forward);
+        Vector3 far_bottomRight = closeCamVertices[0] + (xDistance * transform.right) + (-yDistance * transform.up) + (zDistance * transform.forward);
+        Vector3 far_bottomLeft = closeCamVertices[0] + (-xDistance * transform.right) + (-yDistance * transform.up) + (zDistance * transform.forward);
+
+        return new Vector3[4] {far_topLeft, far_topRight, far_bottomRight, far_bottomLeft};
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(closeCamVertices != null && farCamVertices != null)
         {
-            Gizmos.DrawSphere(vertex, 0.01f);
+            foreach(Vector3 vertex in closeCamVertices)
+                Gizmos.DrawSphere(vertex, 0.01f);
+
+            foreach(Vector3 vertex in farCamVertices)
+                Gizmos.DrawSphere(vertex, 50f);
         }
     }
-    */
 }
