24c9ca2c8ce0b508df99d828b710ebe1dbf7d8f4
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-12-17 04:12:14 +0000

    Add Shape class which will define what a shape is and contains the relevant methods to instantiate and alter them.

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public static ICollection<Shape> shapeList = new List<Shape>{};
+    public ICollection<PlaneOrientation> partialPlanesOrientation = new List<PlaneOrientation>{};
+    public Vector3[] vertices;
+    private new Renderer renderer;
+
+    private void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+        shapeList.Add(this);
+        vertices = gameObject.GetComponent<MeshFilter>().mesh.vertices;
+
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = transform.TransformPoint(vertices[i]);
+        }
+    }
+
+    public void SwitchMaterial(Material newMaterial)
+    {
+        renderer.material = newMaterial;
+    }
+
+    /*public Shape(ICollection<Vector3> vertices)
+    {
+        this.vertices = vertices;
+    }
+    */
+}
